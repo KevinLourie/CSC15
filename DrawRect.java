c4b8db730ed8a5666feb68f566196c704ad217ce
KevinLourie <kzlourie>
2016-03-25 16:58:59 -0700

    Fixed the name

--- DrawRect.java
+++ DrawRect.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-class MyCanvas extends JComponent {
+class DrawRect extends JComponent {
 
   public void paint(Graphics g) {
     g.drawRect (10, 10, 200, 200);  
