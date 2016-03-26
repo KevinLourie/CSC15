f296b7d921a3a38e6224a5c91d2ac32f967b4675
KevinLourie <kzlourie>
2016-03-25 17:11:24 -0700

    Added aspect ratio

--- Circle.java
+++ Circle.java
@@ -1,13 +1,14 @@
 public class Circle { 
-   static final int radius = 10;
+   static final int radius = 20;
+   static final double aspectXY = 3/1.6;
    public static void main(String[] args) {
          // draw rows radius to -radius, counting down by -1
       for (int y = radius; y >= -radius; y--) {
       // row has spaces, stars, newline
-         int circleX = (int)(2*Math.sqrt(radius*radius - y*y)+.5);
+         int circleX = (int)(aspectXY*Math.sqrt(radius*radius - y*y)+.5);
       
       // spaces start at -radius, end at -circleX - 1
-         for (int x = -2*radius; x < -circleX; x++) {
+         for (int x = (int)(-aspectXY*radius-.5); x < -circleX; x++) {
             System.out.print(" ");
          }
       
